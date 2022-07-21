2bc3bf3008190684b01806b136757ad49992ed76
fly <43378440+liyingyanUCAS@users.noreply.github.com>
2022-07-21 11:29:23 +0800

    Update GMW_train.sh

--- GMW/GMW_train.sh
+++ GMW/GMW_train.sh
@@ -2,5 +2,5 @@
 python -m torch.distributed.launch --master_port 33521 --nproc_per_node=4 \
 main.py --log-dir ./logs/big_demo \
 -b 8 --lr 1e-4 --epoch 100 --val_freq 5 \
---train_data_path /mnt/truenas/scratch/yingyan.li/Pairs_Depth/output/kitti/pairs/sync_with_corner_loss_eval_1500_0/gen_data_train.json \
---val_data_path /mnt/truenas/scratch/yingyan.li/Pairs_Depth/output/kitti/pairs/sync_with_corner_loss_eval_1500_0/gen_data_infer.json
+--train_data_path DGDE/gen_data/gen_data_train.json \
+--val_data_path DGDE/gen_data/gen_data_infer.json
